47c740f3d7357346185b287ab0b08975b32eb10e
Almira <almirag812gmail.com>
2024-05-09 19:48:06 +0300

    были добавли комментарии к тем строкам кода, которые отвечают за анимацию

--- core/src/ru/samsung/mygame/MyGdxGame.java
+++ core/src/ru/samsung/mygame/MyGdxGame.java
@@ -2,7 +2,7 @@ package ru.samsung.mygame;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -11,7 +11,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-//import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
@@ -21,7 +21,7 @@ public class MyGdxGame extends ApplicationAdapter {
     Texture textMapTexture;
     Vector2 pos;
     OrthographicCamera camera;
-   // private boolean drawText = true;
+    private boolean drawText = true;
     Texture walkSheet;  // что-то с анимацией
     Animation<TextureRegion> walkAnimation; // что-то с анимацией
     float stateTime; // таймер, чтобы считать сколько прошло между кадрами в анимации
